63141d977cd60aa1d8dbcd31aef438a615190b72
Tory Clasen <ToryClasen@Gmail.com>
2022-12-04 21:39:56 +0000

    refactor: standardize filenames and cset up relative file paths

--- 2022/day01/main_test.ts
+++ 2022/day01/main_test.ts
@@ -1,22 +1,26 @@
 import { assertEquals } from "https://deno.land/std@0.167.0/testing/asserts.ts";
+import * as path from "https://deno.land/std@0.102.0/path/mod.ts";
 import { solution } from "./main.ts";
 
+export const mainModuleDir = path.dirname(path.fromFileUrl(Deno.mainModule));
+
 Deno.test(async function samplePart1Test() {
-  const data = await Deno.readTextFile("sample1.txt");
+  const data = await Deno.readTextFile(mainModuleDir + "/" + "sample1.txt");
   assertEquals(solution(data), 24000);
 });
 
 Deno.test(async function inputPart1Test() {
-  const data = await Deno.readTextFile("input1.txt");
+  const data = await Deno.readTextFile(mainModuleDir + "/" + "input1.txt");
   assertEquals(solution(data), 74198);
 });
 
 Deno.test(async function samplePart2Test() {
-  const data = await Deno.readTextFile("sample1.txt");
+  const data = await Deno.readTextFile(mainModuleDir + "/" + "sample1.txt");
   assertEquals(solution(data, 3), 45000);
 });
 
 Deno.test(async function inputPart2Test() {
-  const data = await Deno.readTextFile("input1.txt");
+  console.log(mainModuleDir + "input1.txt");
+  const data = await Deno.readTextFile(mainModuleDir + "/" + "input1.txt");
   assertEquals(solution(data, 3), 209914);
 });
