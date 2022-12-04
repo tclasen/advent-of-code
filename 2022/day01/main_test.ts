5cc57732782fe5a3c99fb702b3ea43b7d4d310c1
Tory Clasen <ToryClasen@Gmail.com>
2022-12-04 21:41:54 +0000

    fix: filenames

--- 2022/day01/main_test.ts
+++ 2022/day01/main_test.ts
@@ -5,22 +5,22 @@ import { solution } from "./main.ts";
 export const mainModuleDir = path.dirname(path.fromFileUrl(Deno.mainModule));
 
 Deno.test(async function samplePart1Test() {
-  const data = await Deno.readTextFile(mainModuleDir + "/" + "sample1.txt");
+  const data = await Deno.readTextFile(mainModuleDir + "/" + "sample.txt");
   assertEquals(solution(data), 24000);
 });
 
 Deno.test(async function inputPart1Test() {
-  const data = await Deno.readTextFile(mainModuleDir + "/" + "input1.txt");
+  const data = await Deno.readTextFile(mainModuleDir + "/" + "input.txt");
   assertEquals(solution(data), 74198);
 });
 
 Deno.test(async function samplePart2Test() {
-  const data = await Deno.readTextFile(mainModuleDir + "/" + "sample1.txt");
+  const data = await Deno.readTextFile(mainModuleDir + "/" + "sample.txt");
   assertEquals(solution(data, 3), 45000);
 });
 
 Deno.test(async function inputPart2Test() {
   console.log(mainModuleDir + "input1.txt");
-  const data = await Deno.readTextFile(mainModuleDir + "/" + "input1.txt");
+  const data = await Deno.readTextFile(mainModuleDir + "/" + "input.txt");
   assertEquals(solution(data, 3), 209914);
 });
